3a3a4efe0a585e35567f0e3f9600b4f0f714f960
josunefon <jordi.sune@microsoft.com>
2022-02-09 15:16:53 +0100

    bicep

---
+++ toolkits/arm-ttk-master/unit-tests/_arm-ttk-module/JSONC.module.tests.ps1
@@ -0,0 +1,11 @@
+﻿Push-Location $PSScriptRoot 
+
+describe "JSONC" {
+    it "Supports JSONC files" {        
+        Test-AzTemplate -TemplatePath (Join-Path $pwd "Sample.jsonc") -Test 'DeploymentTemplate Schema Is Correct' -File "Sample.jsonc"  |
+            Select-Object -ExpandProperty Passed | 
+            Should -Be $true
+    }
+}
+
+Pop-Location
