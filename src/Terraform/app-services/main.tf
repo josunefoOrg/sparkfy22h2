64906f8f8791425bdb2aa2be40843a5be0f1c3c5
Jose Luis Carrillo Aguilar <jose.carrillo@microsoft.com>
2022-02-16 12:59:38 +0100

    Added

--- src/Terraform/app-services/main.tf
+++ src/Terraform/app-services/main.tf
@@ -6,16 +6,16 @@ terraform {
       version = ">=2.96.0"
     }
   }
-  backend "azurerm" {}
+  backend "azurerm" {
+    storage_account_name = "jojosparkstg"
+    container_name       = "tfstate"
+    key                  = "acr.tfstate"
+  }
 }
 
 provider "azurerm" {
-  subscription_id      = "ed29c799-3b06-4306-971a-202c3c2d29a9"
-  tenant_id            = "72f988bf-86f1-41af-91ab-2d7cd011db47"
-  storage_account_name = "jojosparkstg"
-  container_name       = "tfstate"
-  key                  = "app-services.tfstate"
-  resource_group_name  = "spark-jojo-demo"
+  subscription_id = "ed29c799-3b06-4306-971a-202c3c2d29a9"
+  tenant_id       = "72f988bf-86f1-41af-91ab-2d7cd011db47"
   features {}
 }
 
